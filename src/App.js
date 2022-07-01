3571ef15da443788e0fb1a3ac6067acdf7f5f2e2
iamxxx777 <iamxxx333@gmail.com>
2022-07-01 01:17:22 +0100

    splited bundle with react lazy

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import { useSelector } from 'react-redux';
 import { useState } from 'react';
 import ScrollToTop from './ScrollToTop';
 
-// import { getPosts } from './redux/actions/postActions';
-
-import Sidenav from './components/SideNav/Sidenav';
-import Overlay from './components/Overlay/Overlay';
-
 // Screens
 import Homescreen from "./screens/Homescreen/Homescreen";
 import Postsscreen from './screens/Postsscreen/Postsscreen';
 import Articlescreen from './screens/Articlescreen/Articlescreen';
-import Authorpage from './screens/Authorboard/Authorpage';
-import Register from "./screens/Register/Register";
-import Login from "./screens/Login/Login";
-import Notfound from './screens/Notfound/Notfound';
 
-import './App.css';
+// import { getPosts } from './redux/actions/postActions';
+
+import Sidenav from './components/SideNav/Sidenav';
+import Overlay from './components/Overlay/Overlay';
+import Loading from './components/Loader/Loading';
+
+import "./App.css";
+
+
+// Splited Screens
+const Notfound = React.lazy(() => import("./screens/Notfound/Notfound"));
+const Login = React.lazy(() => import("./screens/Login/Login"));
+const Register = React.lazy(() => import("./screens/Register/Register"));
+const Authorpage = React.lazy(() => import("./screens/Authorboard/Authorpage"));
+
+
+// import Authorpage from './screens/Authorboard/Authorpage';
+// import Register from "./screens/Register/Register";
+// import Login from "./screens/Login/Login";
+// import Notfound from './screens/Notfound/Notfound';
+
 
 
 const App = () => {
@@ -39,16 +50,18 @@ const App = () => {
                 <Overlay show={sideToggle} click={() => setSideToggle(false)} />
                 <Sidenav show={sideToggle} click={() => setSideToggle(false)} />
                 <ScrollToTop>
-                <Switch>
-                    <Route exact path={"/"}> <Homescreen click={() => setSideToggle(true)} /> </Route>
-                    <Route path={"/allposts"}> <Postsscreen click={() => setSideToggle(true)} /> </Route>
-                    <Route path={`/article/:id`}> <Articlescreen click={() => setSideToggle(true)} /> </Route>
-                    <Route path={"/author"}>{user.username ? <Authorpage /> : <Redirect to="/login" /> } </Route>
-                    <Route path={"/register"}> {!user.username ? <Register click={() => setSideToggle(true)} /> : <Redirect to="/author" /> } </Route>
-                    <Route path={"/login"}> <Login click={() => setSideToggle(true)} /> </Route>
-                    <Route path="/404"> <Notfound click={() => setSideToggle(true)} /> </Route>
-                    <Redirect from="*" to="/404" />
-                </Switch> 
+                    <React.Suspense fallback={<Loading />}>
+                        <Switch>
+                            <Route exact path={"/"}> <Homescreen click={() => setSideToggle(true)} /> </Route>
+                            <Route path={"/allposts"}> <Postsscreen click={() => setSideToggle(true)} /> </Route>
+                            <Route path={`/article/:id`}> <Articlescreen click={() => setSideToggle(true)} /> </Route>
+                            <Route path={"/author"}>{user.username ? <Authorpage /> : <Redirect to="/login" /> } </Route>
+                            <Route path={"/register"}> {!user.username ? <Register click={() => setSideToggle(true)} /> : <Redirect to="/author" /> } </Route>
+                            <Route path={"/login"}> <Login click={() => setSideToggle(true)} /> </Route>
+                            <Route path="/404"> <Notfound click={() => setSideToggle(true)} /> </Route>
+                            <Redirect from="*" to="/404" />
+                        </Switch> 
+                    </React.Suspense>
                 </ScrollToTop>
             </div>
         </Router> 
